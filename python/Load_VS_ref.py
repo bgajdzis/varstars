5c97578551aa7a8d2ea9acd9d207911d51c68689
Bartlomiej Gajdzis <gajdzis@Agamemnon.localdomain>
2017-11-18 17:07:06 +0100

    notes and checks regarding filename format

--- python/Load_VS_ref.py
+++ python/Load_VS_ref.py
@@ -48,8 +48,7 @@ from cesium.features.period_folding import (period_folding, get_fold2P_slope_per
 from cesium.features.scatter_res_raw import scatter_res_raw
 
 def load(filename):
-    if not (filename.endswith('.dat') and len(filename.split('/')[-1].split('-')) == 4):
-        raise Exception('Wrong filename')
+    assert (filename.endswith('.dat') and len(filename.split('/')[-1].split('-')) == 4), 'Filename in wrong format (see README)'
     try:
         with open(filename,"r") as f:
             df = pd.read_table(f,delim_whitespace = True,names=['time','intensity','error'],dtype={'time':np.float64,'intensity':np.float64,'error':np.float64}) # Import data from TSV file
