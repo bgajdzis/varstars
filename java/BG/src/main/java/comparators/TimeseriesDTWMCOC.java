a7c8a70317071c5abcbc4f79af103bd72bbc1c02
Bartlomiej Gajdzis <gajdzis@Agamemnon.localdomain>
2017-12-09 13:39:58 +0100

    fixed typos in TimeseriesDTWMCOC

--- java/BG/src/main/java/comparators/TimeseriesDTWMCOC.java
+++ java/BG/src/main/java/comparators/TimeseriesDTWMCOC.java
@@ -1,55 +1,41 @@
 package comparators;
 
-import com.fastdtw.dtw.FastDTW;
 import com.fastdtw.dtw.TimeWarpInfo;
 import com.fastdtw.timeseries.TimeSeries;
 import com.fastdtw.timeseries.TimeSeriesBase;
-import com.fastdtw.util.Distances;
 import general.*;
 import pl.ls.comparators.ComparatorEnums;
 import pl.ls.comparators.base.monolithic.AbstractMonolithicCOComparator;
 import pl.ls.exceptions.ComparatorException;
 import pl.ls.objects.compound.base.monolithic.IMonolithicReferenceObject;
+import com.fastdtw.util.Distances;
+import com.fastdtw.dtw.FastDTW;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.Integer.max;
 import static java.lang.Integer.min;
 
 public class TimeseriesDTWMCOC extends AbstractMonolithicCOComparator<VarstarFeatureSet> {
 
 
-Exception e
-
-{
-        if (((VarstarsRef) ref).getReferenceName() != "NPer") {
-            this.p = Constants.perP;
-        }
-    }
-
-    {
-        //log.error(e, e);
-        return 0;
-    }
-
-    @Override
-        try
-
-        public TimeseriesDTWMCOC(String name) throws ComparatorException {
+    public TimeseriesDTWMCOC(String name) throws ComparatorException {
         super(name);
-    } catch(
+    }
 
     @Override
     public Integer getUniqueId() {
         return Constants.TimeseriesDTW;
-    })
+    }
 
     @Override
     public ComparatorEnums.ComparatorUnitCost getUnitCost() {
         return ComparatorEnums.ComparatorUnitCost.MEDIUM;
     }
 
+    @Override
     protected void setP() {
         this.p = Constants.nonPerP;
     }
@@ -59,35 +45,41 @@ Exception e
         Map<Double, Double> inputValue;
         Map<Double, Double> referenceValue;
         try {
+            if (((VarstarsRef) ref).getReferenceName() != "NPer") {
+                this.p = Constants.perP;
+            }
+        } catch (Exception e) {
+            //log.error(e, e);
+            return 0;
+        }
+        try {
             inputValue = ((VarstarsCO) getInputObject()).getTimeseries();
             Double peri = ((VarstarsCO) getInputObject()).getPeriodFast();
             referenceValue = ((VarstarsRef) ref).getTimeseries();
             Double perr = ((VarstarsRef) ref).getPeriodFast();
-            int len = min(referenceValue.size(), inputValue.size());
+            int len = min(referenceValue.size(),inputValue.size());
             TimeSeriesBase.Builder tsb1 = TimeSeriesBase.builder();
             TimeSeriesBase.Builder tsb2 = TimeSeriesBase.builder();
-            List<Map.Entry<Double, Double>> pointsi = new ArrayList<Map.Entry<Double, Double>>(inputValue.entrySet());
-            List<Map.Entry<Double, Double>> pointsr = new ArrayList<Map.Entry<Double, Double>>(referenceValue.entrySet());
-            pointsi = Utils.listSample(pointsi, len);
-            pointsr = Utils.listSample(pointsr, len);
+            List<Map.Entry<Double,Double>> pointsi = new ArrayList<Map.Entry<Double,Double>>(inputValue.entrySet());
+            List<Map.Entry<Double,Double>> pointsr = new ArrayList<Map.Entry<Double,Double>>(referenceValue.entrySet());
+            pointsi = Utils.listSample(pointsi,len);
+            pointsr = Utils.listSample(pointsr,len);
             pointsi = Utils.listNormalize(pointsi);
             pointsr = Utils.listNormalize(pointsr);
-            for (Map.Entry<Double, Double> entry : pointsi) {
-                tsb1.add(entry.getKey() % peri, entry.getValue());
+            for (Map.Entry<Double,Double> entry : pointsi) {
+                tsb1.add(entry.getKey()%peri,entry.getValue());
             }
             TimeSeries ts1 = tsb1.build();
-            for (Map.Entry<Double, Double> entry : pointsr) {
-                tsb2.add(entry.getKey() % perr, entry.getValue());
+            for (Map.Entry<Double,Double> entry : pointsr) {
+                tsb2.add(entry.getKey()%perr,entry.getValue());
             }
             TimeSeries ts2 = tsb2.build();
             TimeWarpInfo tw = FastDTW.compare(ts1, ts2, 10, Distances.EUCLIDEAN_DISTANCE);
             Double dist = tw.getDistance();
-            Double sim = 1 - dist / len;
+            Double sim = 1 - dist/len;
             //System.out.println(len);
             //System.out.println(tw.toString());
-            if (sim > 1 || sim < 0) {
-                System.out.println("źle");
-            }
+            if (sim > 1 || sim < 0) {System.out.println("źle"); }
             return sim;
         } catch (Exception e) {
             //log.error(e,e);
